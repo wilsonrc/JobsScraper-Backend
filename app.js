2bb15117137586b32e67009abc0aa9bc56618b21
Vyzaldy Sanchez <vyzaldysanchez@gmail.com>
2018-10-27 01:36:10 -0400

    Adds initial routes

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
+'use strict';
+
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+
 import db from './db';
+import { assignRoutes } from './routes';
 
 const app = express();
+const router = express.Router();
+const PORT = process.env.PORT || 5000;
 
-// Testing DB connection
-db
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.listen(3000, () => console.log('Scrapper backend started on port 3000'));
+assignRoutes(app, router);
+
+(async () => {
+  try {
+    await db.sync(); // Waiting for all models and DB resources to load properly
+  } catch (e) {
+    console.error('Unable to sync the database:', e);
+  }
+
+  app.listen(PORT, () => console.log(`Scrapper backend started on port ${PORT}`));
+})();
