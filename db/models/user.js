9451e58b652d1b69d4bc648169811f497546c414
Vyzaldy Sanchez <vyzaldysanchez@gmail.com>
2018-10-27 10:44:03 -0400

    Adds missing User fields

--- db/models/user.js
+++ db/models/user.js
@@ -2,11 +2,32 @@
 
 export default (sequelize, DataTypes) => {
   return sequelize.define('users', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      allowNull: false,
+      defaultValue: DataTypes.UUIDV4,
+    },
     firstName: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     lastName: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+    },
+    deletedAt: {
+      type: DataTypes.DATE,
     }
   },
   {
