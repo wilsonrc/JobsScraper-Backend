b659f3a37c52975e81cc96c54ac1850489dfa679
Vyzaldy Andrés Sanchez <vyzaldysanchez@gmail.com>
2019-03-30 02:10:40 -0400

    Adds correct linter for .js files

--- db/migrations/20181027183232-add-job_posts-table.js
+++ db/migrations/20181027183232-add-job_posts-table.js
@@ -1,75 +1,68 @@
-'use strict';
-
 module.exports = {
-  up: (queryInterface, Sequelize) =>
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+  /*
+    Add altering commands here.
+    Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    Example:
+    return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+  */
+  up: (queryInterface, Sequelize) => queryInterface.createTable('job_posts', {
+    id: {
+      type: Sequelize.UUID,
+      primaryKey: true,
+      allowNull: false,
+      defaultValue: Sequelize.UUIDV4,
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    company_id: {
+      type: Sequelize.UUID,
+      allowNull: false,
+      references: {
+        model: 'companies',
+        key: 'id',
+      },
+    },
+    howToApply: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    isRemote: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+    },
+    location: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    postedAt: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    createdAt: {
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      type: Sequelize.DATE,
+    },
+    deletedAt: {
+      type: Sequelize.DATE,
+    },
+  }),
 
-     queryInterface.createTable('job_posts', {
-      id: {
-        type: Sequelize.UUID,
-        primaryKey: true,
-        allowNull: false,
-        defaultValue: Sequelize.UUIDV4,
-      },
-      title: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      description: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      company_id: {
-        type: Sequelize.UUID,
-        allowNull: false,
-        references: {
-          model: 'companies',
-          key: 'id',
-        },
-      },
-      howToApply: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      isRemote: {
-        type: Sequelize.BOOLEAN,
-        allowNull: false,
-      },
-      location: {
-        type: Sequelize.STRING,
-        allowNull: true,
-      },
-      postedAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      createdAt: {
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        type: Sequelize.DATE,
-      },
-      deletedAt: {
-        type: Sequelize.DATE,
-      },
-    })
-  ,
+  /*
+    Add reverting commands here.
+    Return a promise to correctly handle asynchronicity.
 
-  down: queryInterface =>
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
-
-     queryInterface.dropTable('job_posts')
+    Example:
+    return queryInterface.dropTable('users');
+  */
+  down: queryInterface => queryInterface.dropTable('job_posts')
   ,
 };
